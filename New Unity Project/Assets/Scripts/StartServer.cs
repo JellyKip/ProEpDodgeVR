5843136845920665b1a13e67be934b8467cf11a6
Kip <waitam93@hotmail.com>
2016-12-15 19:17:49 +0100

    no more need for Networkmanager Hud
    but need to be able to change the localhost to an actual IP adress

--- New Unity Project/Assets/Scripts/StartServer.cs
+++ New Unity Project/Assets/Scripts/StartServer.cs
@@ -1,23 +1,37 @@
 ﻿using UnityEngine;
 using System.Collections;
 using UnityEngine.Networking;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
-public class StartServer : MonoBehaviour {
+public class StartServer : NetworkBehaviour {
 
-    string HostName = "198.168.178.1";
-    
+    string HostName = "127.0.0.1";
+    public Button b1;
+    public Button b2;
 
     // Use this for initialization
+    private void Start()
+    {
+        b1.GetComponent<Button>();
+        b2.GetComponent<Button>();
+    }
     public void StartHost () {
+      //  NetworkManager.singleton.serverBindToIP = true;
+      //  NetworkManager.singleton.serverBindAddress = Network.player.ipAddress;
         NetworkManager.singleton.networkPort = 7777;
         NetworkManager.singleton.maxConnections = 4;
         NetworkManager.singleton.StartHost();
+        
+        b1.gameObject.SetActive(false);
+            
     }
 	
-	// Update is called once per frame
 	public void JoinWorld () {
         NetworkManager.singleton.networkAddress = HostName;
         NetworkManager.singleton.networkPort = 7777;
         NetworkManager.singleton.StartClient();
+        b2.gameObject.SetActive(false);
+
     }
 }
