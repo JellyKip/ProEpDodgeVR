cac2932dc36f3d56ce86cbd69c6fc29f5685876e
Zair Thiel <z_thiel@hotmail.com>
2016-12-16 02:41:59 +0100

    BugFixes
    ket altering the camera rig to load seperate views to no luck

---
+++ New Unity Project/Assets/CameraController.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEngine.Networking;
+using System.Collections;
+
+public class CameraController : NetworkBehaviour {
+
+    [SerializeField]
+    Camera PlayerCam;
+	// Use this for initialization
+	void Start ()
+    {
+        if(!isLocalPlayer)
+        {
+            GetComponent<PlayerController>().enabled = true;
+            PlayerCam.enabled = true;
+        }
+	
+       
+	}
+	
+}
