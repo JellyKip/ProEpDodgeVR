8fc3b0e5d6db888f60f26420808da5da5ec7b523
Zair Thiel <z_thiel@hotmail.com>
2016-12-21 12:47:54 +0100

    small things

---
+++ New Unity Project/Assets/Scripts/CameraControlNet.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Networking;
+
+public class CameraControlNet : NetworkBehaviour {
+
+    public Camera cam;
+
+	
+	// Update is called once per frame
+	void Update () {
+        if (!isLocalPlayer)
+        {
+            cam.enabled = false;
+            return;
+        }
+	}
+}
